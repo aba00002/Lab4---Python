cbcf60a6a47f0dfce46d12132ae492be8dbd845b
aba00002 <aba00002@algonquinlive.com>
2019-10-03 19:30:08 +0100

    slight changes

--- aba00002Library.py
+++ aba00002Library.py
@@ -2,13 +2,10 @@
 #Where M is miles driven and G is gallon used
 
 
-def caculateMpg(Miles, Gallon):
+def calculateMpg(Miles, Gallon):
     MPG = (Miles / Gallon)
     return MPG
 
-Miles = float(input("enter the number of miles driven"))
-Gallon = float(input("enter the number of gallons used"))
-print("Dear driver, the mile per gallon rate of your car is", caculateMpg(Miles, Gallon))
 
 
 
@@ -22,9 +19,6 @@ def calculateAreaOfCircle(radius):
     Area = math.pi * (radius ** 2)
     return Area
 
-import math
-radius = float(input("Enter the radius of the circle"))
-print("Dear user, the radius of your circle is", calculateAreaOfCircle(radius))
 
 
 
@@ -38,8 +32,7 @@ def convertFahrenheitToCelsius(TC):
     TF = TC * (9 / 5) + 32
     return TF
 
-TC = float(input("What is the temperature in celsius?"))
-print("Dear user, The temperature in fahrenheit equivalent of your entry is", convertFahrenheitToCelsius(TC))
+
 
 
 
@@ -52,9 +45,3 @@ def DistanceBetweenPoints(x,x1,y,y1):
     Distancehere = math.sqrt((x - x1)**2 - (y - y1)**2)
     return Distancehere
 
-import math
-x = int(input("What is the cordinate of x?"))
-x1 = int(input("What is the cordinate of x1?"))
-y = int(input("What is the cordinate of y?"))
-y1 = int(input("What is the cordinate of y1?"))
-print("The distance between the two points is", DistanceBetweenPoints(x,x1,y,y1))
--- testPrograms.py
+++ testPrograms.py
@@ -1,1 +1,31 @@
-from aba00002Library import caculateMpg 
+from aba00002Library import calculateMpg 
+m = float(input("enter the number of miles driven"))
+g = float(input("enter the number of gallons used"))
+mpg = calculateMpg(m, g)
+print("Dear driver, the mile per gallon rate of your car is", mpg)
+
+
+
+from aba00002Library import calculateAreaOfCircle
+import math
+r = float(input("Enter the radius of the circle"))
+A = calculateAreaOfCircle(r)
+print("Dear user, the Area of your circle is", A )
+
+
+
+from aba00002Library import convertFahrenheitToCelsius
+TC = float(input("What is the temperature in celsius?"))
+F = convertFahrenheitToCelsius(TC)
+print("Dear user, The temperature in fahrenheit equivalent of your entry is", F)
+
+
+
+from aba00002Library import DistanceBetweenPoints
+import math
+x = int(input("What is the cordinate of x?"))
+x1 = int(input("What is the cordinate of x1?"))
+y = int(input("What is the cordinate of y?"))
+y1 = int(input("What is the cordinate of y1?"))
+D = DistanceBetweenPoints(x,x1,y,y1)
+print("The distance between the two points is", D)
